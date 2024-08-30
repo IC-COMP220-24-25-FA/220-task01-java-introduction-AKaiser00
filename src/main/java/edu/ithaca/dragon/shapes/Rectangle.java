804dd09b193ffbd84dd1ce5e477eb58b386c9e66
AKaiser00 <andrewkaiser05@gmail.com>
2024-08-29 22:45:12 -0400

    added javadoc to all methods in Rectangle.java

--- src/main/java/edu/ithaca/dragon/shapes/Rectangle.java
+++ src/main/java/edu/ithaca/dragon/shapes/Rectangle.java
@@ -5,26 +5,44 @@ public class Rectangle {
     private int length;
     private int width;
 
+    /**
+     * @throws IllegalArgumentException if length or width is not a positive number
+     */
     public Rectangle(double length, double width){
         throw new RuntimeException("Not implemented yet");
     }
 
+    /**
+     * @return the area of the rectangle
+     */
     public double calcArea(){
        throw new RuntimeException("Not implemented yet");
     }
 
+    /**
+     * @post doubles the size of the rectangle
+     */
     public void doubleSize(){
         throw new RuntimeException("Not implemented yet");
     }
 
+    /**
+     * @return the longest straight line that can be drawn in the rectangle
+     */
     public double longestLineWithin(){
         throw new RuntimeException("Not implemented yet");
     }
     
+    /**
+     * @return length of the rectangle
+     */
     public double getLength(){
         throw new RuntimeException("Not implemented yet");
     }
     
+    /**
+     * @return width of the rectangle
+     */
     public double getWidth(){
         throw new RuntimeException("Not implemented yet");
     }
--- src/test/java/edu/ithaca/dragon/shapes/RectangleTest.java
+++ src/test/java/edu/ithaca/dragon/shapes/RectangleTest.java
@@ -8,11 +8,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class RectangleTest {
-    
+
     @Test
     public void sampleTestThatPasses(){
         assertEquals("This test should pass", "This test should pass");
         assertTrue(8>5);
         assertFalse(8<5);
     }
+    
 }
